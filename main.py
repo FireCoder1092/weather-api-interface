6b2557c431d4eb2f93aae60c40bf48528914db17
FireCoder1092 <smaranpreethesh@gmail.com>
2025-08-17 18:40:55 +1200

    Finish the app

--- main.py
+++ main.py
@@ -10,7 +10,25 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"The {option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+            st.info("This is not a city or a place, please put in a real place")
+
+
+        if option == "Sky":
+            images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png",
+                      "Rain":"images/rain.png", "Snow":"images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            captions = [dict["dt_txt"] for dict in filtered_data]
+            for img, caption in zip(image_paths, captions):
+                st.image(img, width=115, caption=caption)
+    except KeyError:
+        st.info("This place is not a existing place. Maybe you have made a typo? Please put in an existing city or place.")
